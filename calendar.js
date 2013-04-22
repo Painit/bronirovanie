fd94f874def7fdda00994c12f2fb571eaafc5d4d
zabutok <zabutok@mail.ru>
2013-04-22 14:56:14 +0400

    Функция пространства имен

--- calendar.js
+++ calendar.js
@@ -4,3 +4,23 @@
  * Time: 14:20
  * To change this template use File | Settings | File Templates.
  */
+var SNN = SNN || {};
+//пространство имен
+SNN.namespace = function (ns) {
+    var parts = ns.split('.'),
+        parent = SNN,
+        i;
+    //удаляем префикс
+    if (parts[0] === 'SNN') {
+        parts = parts.slice(1);
+    }
+
+    for (i = 0;i < parts.length; i += 1) {
+        //создаем свойствое если его нет
+        if (typeof parent[parts[i]] === "undefined") {
+            parent[parts[i]] = {};
+        }
+        parent = [parts[i]]
+    }
+    return parent;
+};
